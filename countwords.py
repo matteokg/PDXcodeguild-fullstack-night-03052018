dac454eeda34594c73fb10cfc55338c7df046fb8
matteokg <matteokgouveia@gmail.com>
2018-03-20 19:30:52 -0700

    added word counter and book.

---
+++ countwords.py
@@ -0,0 +1,24 @@
+
+import string
+
+
+
+wordcount = {}
+with open ("frankenstein.txt") as frank:
+    contents = frank.read()
+    stripped = contents.lower().translate(str.maketrans({a:None for a in string.punctuation})).split()
+    stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
+
+
+
+
+    for w in stripped:
+        if w in wordcount and w not in stopwords:
+            wordcount[w] += 1
+        else:
+            wordcount[w] = 1
+
+    words = list(wordcount.items())
+    words.sort(key=lambda tup: tup[1], reverse=True)
+    for i in range(min(10, len(words))):
+        print(words[i])
